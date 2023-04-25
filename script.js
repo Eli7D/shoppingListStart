9b84b1f0ddb9c3e12149033e94e7a61f41ddf644
Elisheva Dratler <elishevadratler@gmail.com>
2023-04-25 11:13:16 +0300

    dbl click

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const logo = document.querySelector("title");
 
 const onclick = () => console.log("click event");
+const ondoubleclick = () => console.log("doubleclick event");
 
 logo.addEventListener("click", onclick);
+logo.addEventListener("dblclick", ondoubleclick);
